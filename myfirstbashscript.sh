2f4815b37f23e6ac469ba1c5fa9ca08fe02f6ce9
Viktoriia_Mariella <boychenkovika1985@gmail.com>
2025-11-22 17:50:35 +0100

    deleted the file: myfirstbashscript.sh

--- myfirstbashscript.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Hello $USER"
-date
-echo $PWD
-process_bioset=$(ps -ef | grep -v grep | grep bioset | wc -l)
-echo "bioset process count $process_bioset"
-ls -la /etc/passwd | awk '{print $1}' 
-
